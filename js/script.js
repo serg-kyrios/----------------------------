50d2b18ef2a2f3358c3f21ad5a6dd8ceb618a7da
serg-kyrios <sergkyrios@gmail.com>
2023-07-21 21:24:32 +0300

    Form w two

--- js/script.js
+++ js/script.js
@@ -94,8 +94,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
-          modal = document.querySelector('.modal'),
-          modalClousBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal'),
+        modalClousBtn = document.querySelector('[data-close]');
 
     function openModal() {
         modal.classList.add('show');
@@ -103,7 +103,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.style.overflow = 'hidden';
         clearInterval(modalTimerId);//При відкритті мод.вікна в ручну-не вийде автоматом
     }
-    
+
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', openModal);
     });
@@ -128,46 +128,46 @@ window.addEventListener('DOMContentLoaded', () => {
             clouseModal();
         }
     });
-    
-     const modalTimerId = setTimeout(openModal, 30000);
+
+    const modalTimerId = setTimeout(openModal, 30000);
 
     function showModalByScroll() {
-    
-        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1 ) {
+
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
             openModal();
             window.removeEventListener('scroll', showModalByScroll);
         }
     }
     window.addEventListener('scroll', showModalByScroll);
 
-// Класси для карточок
+    // Класси для карточок
 
-class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-        this.src = src;
-        this.alt = alt;
-        this.title = title;
-        this.descr = descr;
-        this.price = price;
-        this.classes = classes; // REST
-        this.parent = document.querySelector(parentSelector);
-        this.transfer = 27;
-        this.changeToUAH(); 
-    }
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes; // REST
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
 
-    changeToUAH() {
-        this.price = this.price * this.transfer; 
-    }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
 
-    render() {
-        const element = document.createElement('div');
-        if (this.classes.length === 0) {
-            this.element = 'menu__item';
-            element.classList.add(this.element);
-        } else {
-            this.classes.forEach(className => element.classList.add(className));
-        }               
-        element.innerHTML = `
+        render() {
+            const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            element.innerHTML = `
                 <img src=${this.src} alt=${this.alt}>
                 <h3 class="menu__item-subtitle">${this.title}</h3>
                 <div class="menu__item-descr">${this.descr}</div>
@@ -177,120 +177,91 @@ class MenuCard {
                     <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
         `;
-        this.parent.append(element);
+            this.parent.append(element);
+        }
     }
-}
 
-new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container",
-    'menu__item'
-).render();
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container",
+        'menu__item'
+    ).render();
 
-new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    14,
-    ".menu .container",
-    'menu__item'
-).render();
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container",
+        'menu__item'
+    ).render();
 
-new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    ".menu .container",
-    'menu__item'
-).render();
-        
-  //   ).render(); Зроблено без перемінної, бо можна. Все одно її більше ніде не видно.
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container",
+        'menu__item'
+    ).render();
 
-  //Forms
+    //     ).render(); Зроблено без перемінної, бо можна. Все одно її більше ніде не видно.
 
- // Forms
+    //   Forms
 
- const forms = document.querySelectorAll('form');
- const message = {
-     loading: 'Загрузка...',
-     success: 'Спасибо! Скоро мы с вами свяжемся',
-     failure: 'Что-то пошло не так...'
- };
+    //  Forms
 
- forms.forEach(item => {
-     postData(item);
- });
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
 
- function postData(form) {
-     form.addEventListener('submit', (e) => {
-         e.preventDefault();
+    forms.forEach(item => {
+        postData(item);
+    });
 
-         let statusMessage = document.createElement('div');
-         statusMessage.classList.add('status');
-         statusMessage.textContent = message.loading;
-         form.appendChild(statusMessage);
-     
-         const request = new XMLHttpRequest();
-         request.open('POST', 'server.php');
-         request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-         const formData = new FormData(form);
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
 
-         const object = {};
-         formData.forEach(function(value, key){
-             object[key] = value;
-         });
-         const json = JSON.stringify(object);
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.appendChild(statusMessage);
 
-         request.send(json);
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            const formData = new FormData(form);
 
-         request.addEventListener('load', () => {
-             if (request.status === 200) {
-                 console.log(request.response);
-                 statusMessage.textContent = message.success;
-                 form.reset();
-                 setTimeout(() => {
-                     statusMessage.remove();
-                 }, 2000);
-             } else {
-                 statusMessage.textContent = message.failure;
-             }
-         });
-     });
- }
+            const object = {};
+            formData.forEach(function (value, key) {
+                object[key] = value;
+            });
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
-
-
-
-
-// setTimeout(  
-//     () => {
-//       console.log('Hello after 4 seconds');
-//     },
-//     4 * 1000
-//   );
-
-//   const func = () => {
-//     console.log('Hello after 4 seconds');
-//   };
-//   setTimeout(func, 4 * 1000);
-
-//   // example2.js
-// const rocks = who => {
-//     console.log(who + ' rocks');
-//   };
-//   setTimeout(rocks, 2 * 1000, 'Node.js');
-
-//   // solution1.js
-// const theOneFunc = delay => {
-//     console.log('Hello after ' + delay + ' seconds');
-//   };
-//   setTimeout(theOneFunc, 4 * 1000, 4);
-//   setTimeout(theOneFunc, 8 * 1000, 8);
-// const timerId = setInterval(func, 2000);
